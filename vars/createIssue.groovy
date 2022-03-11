7eef65b537b6b22b5dd0d58813f0838f1e01d5d1
Owais Kazi <owaiskazi19@gmail.com>
2022-03-11 19:43:01 +0000

    Created groovy method to create issue on github
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- vars/createIssue.groovy
+++ vars/createIssue.groovy
@@ -1,3 +1,12 @@
 void call(Map args = [:]) {
-    
+    String ISSUE_TITLE = "Jenkins Randomized Gradle Check Failed on"
+    withCredentials([usernamePassword(credentialsId: "${GITHUB_BOT_TOKEN_NAME}")]) {
+        sh([
+            'curl',
+            '-XPOST',
+            '--header "Content-Type: text/x-markdown; charset=utf-8"',
+            "--data '{\"title\":\"'${ISSUE_TITLE}'\",\"body\":\"@opensearch-project/opensearch-coreteam,pleasetakealookatBuild'${BUILD_NUMBER}'ingradle_checklogformoreinformation.\",\"labels\":[\"cicd\",\"bug\"]}'",
+            "https://api.github.com/repos/owaiskazi19/S3Repo/issues?state=all"
+        ])
+    }
 }
